ac39fae8369f3bcd95e2a7a9d3ac79981cc08726
Lucy Awrey <35198881+lucyawrey@users.noreply.github.com>
2024-11-22 03:22:19 -0500

    added ssr preact for fun

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import { Elysia } from "elysia";
 import { databaseUrl, encryptionKey } from "lib/env";
 import { userController } from "controllers/user";
 import { AuthService } from "services/auth";
+import render from "preact-render-to-string";
 
 if (databaseUrl == undefined || encryptionKey == undefined) {
   console.error("  Missing required enviornment variables, stopping server.");
@@ -16,19 +17,17 @@ const app = new Elysia()
     "/",
     ({ set, auth }) => {
       set.headers["content-type"] = "text/html; charset=utf8";
-      const text = auth.isAuthenticated
-        ? `<p>Current logged in user is: ${auth.user.displayName}!</p>`
-        : "";
-      return `
-    <!DOCTYPE html>
-    <html>
-      <body style="background:black; color:white;">
-        <h1>Lantern Tabletop</h1>
-        <p>Welcome to Lantern's API service. Go to <a href="/docs">interactive API documentation</a>.</p>
-        ${text}
-      </body>
-    </html>
-    `;
+      const html = render(
+        <body style="background:black; color:white;">
+          <h1>Lantern Tabletop</h1>
+          <p>
+            Welcome to Lantern's API service. Go to
+            <a href="/docs">interactive API documentation</a>.
+          </p>
+          {auth.isAuthenticated ? <p>Current logged in user is: {auth.user.displayName}!</p> : ""}
+        </body>
+      );
+      return `<!DOCTYPE html><html>${html}</html>`;
     },
     { authenticate: {} }
   )
