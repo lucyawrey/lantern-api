265e0136bac4169d2a158f0bc1c8e2a72c3ada81
Lucy Awrey <35198881+lucyawrey@users.noreply.github.com>
2025-08-21 11:42:00 -0400

    removed all existing code from new branch

--- src/types/enums.ts
+++
@@ -1,9 +0,0 @@
-import { Static, t } from "elysia";
-
-export const Group = t.UnionEnum(["user", "admin"]);
-export const Visibility = t.UnionEnum(["public", "private", "limited", "friends"]);
-
-export type Group = Static<typeof Group>;
-export type Visibility = Static<typeof Visibility>;
-
-export * from "gen/enums";
