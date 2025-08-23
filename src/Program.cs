41cbdbec7bd5eecf4854959ca0fa1d41947b9181
Lucy Awrey <35198881+lucyawrey@users.noreply.github.com>
2025-08-22 23:20:54 -0400

    got basic implementation of content working

--- src/Program.cs
+++ src/Program.cs
@@ -5,6 +5,20 @@ var builder = WebApplication.CreateBuilder(args);
 
 var idGenerator = new IdGenerator(0);
 var db = new LanternContext(idGenerator);
+db.Database.EnsureCreated();
+if (!db.User.Any(u => u.Name == "admin"))
+{
+    var user = new User
+    {
+        Name = "admin",
+        DisplayName = "Lantern Administrator",
+        Groups = [UserGroup.Admin],
+        PasswordHash = "TODOPASSWORDHASHING",
+    };
+    db.User.Add(user);
+    await db.SaveChangesAsync();
+    Console.WriteLine("Default user: admin");
+}
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -27,15 +41,19 @@ app.MapGet("/", () =>
     return "Lantern API";
 });
 
-app.MapPost("/test", async () =>
+app.MapPost("/content", async () =>
 {
-    var test = new Test
+    var content = new Content
     {
-        Name = ""
+        Name = "Fireball",
+        Owner = db.User.Where(u => u.Name == "admin").First(),
+        DataIndexes = [],
+        DataIndexKeys = [],
+        Data = new Dictionary<string, string>(),
     };
-    db.Test.Add(test);
+    db.Content.Add(content);
     await db.SaveChangesAsync();
-    return "Added new Test!";
+    return "Added new content!";
 })
 .WithName("NewTest")
 .WithOpenApi();
